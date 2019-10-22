b562cfc7206c87e9a7c4eea595327636890c9e80
Gianlucator <gianluca.principini@gmail.com>
2019-10-22 19:30:14 +0200

    Modifiche al learning rate del critic e ripristino di log likelihood con entropia

--- actor_critic_keras.py
+++ actor_critic_keras.py
@@ -8,12 +8,15 @@ import pickle
 import numpy as np
 from math import log
 import cv2
+from collections import deque
+import matplotlib.pyplot as plt
+
 
 stack_size = 4
 
 class Agent(object):
     def __init__(self, alpha, beta, gamma=0.99, n_actions=4,
-                 layer1_size=64, layer2_size=64, input_dims=8, is_ram = True, env_name = ''):
+                 layer1_size=256, layer2_size=256, input_dims=8, env_name = ''):
         self.gamma = gamma
         self.alpha = alpha
         self.beta = beta
@@ -21,51 +24,61 @@ class Agent(object):
         self.fc1_dims = layer1_size
         self.fc2_dims = layer2_size
         self.n_actions = n_actions
-        self.is_ram = is_ram
         self.score_history = []
+        self.stacked_frames = deque([np.zeros((76, 84), dtype=np.int) for i in range(stack_size)], maxlen=4)
         self.actor, self.critic, self.policy = self.build_actor_critic_network(env_name)
         self.action_space = [i for i in range(n_actions)]
 
-    # def stack_frames(stacked_frames, state, is_newEpisonde):
-    #     stack_size = 4
-    #     frame = preprocess(state)
-    #     if is_newEpisonde:
-    #         #Clear our stack
-    #         stacked_frames = deque([np.zeros((110, 84), dtype= np.int) for i in range(stack_size)], maxlen=4)
-
-    #         #since qu're in a new episode copy the same frame 4x
-    #         stacked_frames.append(frame)
-    #         stacked_frames.append(frame)
-    #         stacked_frames.append(frame)
-    #         stacked_frames.append(frame)
-
-    #         stacked_state = np.stack(stacked_frames, axis=2)
-    #     else:
-    #         stacked_frames.append(frame)
-    #         stacked_state = np.stack(stacked_frames, axis=2)
-    #     return stacked_state, stacked_frames
-
+    def initialize_stacked_frames(self, initial_frame):
+        frame = self.preprocess(initial_frame)
+        for i in range(stack_size):
+            stacked_frames.append(frame)
+        
+        stacked_state = np.stack(stacked_frames, axis = 2)
+        return stacked_state
+            
 
     def preprocess(self, observation):
         retObs = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
-        retObs = retObs[26:110,:]
+        retObs = retObs[9:102,:]
         ret, retObs = cv2.threshold(retObs,1,255,cv2.THRESH_BINARY)
-        return np.reshape(retObs,(84,84,1))
+        # plt.imshow(np.squeeze(retObs))
+        # plt.show()
+        return np.reshape(retObs / 255,(93,84,1))
+
+    def stack_frames(self, state, isNewEpisode = False):
+        frame = self.preprocess(state)
+        if isNewEpisode:
+            #Clear our stack
+            self.stacked_frames = deque([np.zeros((93, 84), dtype= np.int) for i in range(stack_size)], maxlen=stack_size)
+
+            #since qu're in a new episode copy the same frame 4x
+            self.stacked_frames.append(frame)
+            self.stacked_frames.append(frame)
+            self.stacked_frames.append(frame)
+            self.stacked_frames.append(frame)
+
+            stacked_state = np.stack(self.stacked_frames, axis=2)
+            stacked_frames = self.stacked_frames
+        else:
+            stacked_frames = self.stacked_frames
+            stacked_frames.append(frame)
+            stacked_state = np.stack(stacked_frames, axis=2)
+        
+        out = np.reshape(stacked_state,(93, 84, 4))
+        return out, stacked_frames
 
     def build_actor_critic_network(self, env_name):
-        input = Input(shape=(84, 84, 1))
-        delta = Input(shape=[1])
-        input_tail_network = input
-        if (not self.is_ram):
-            head = Conv2D(64, kernel_size=3, activation='relu')(input)
-            conv1 = Conv2D(64, kernel_size=3, activation='relu')(head)
-            input_tail_network = Flatten()(conv1)
+        input = Input(shape=(93, 84, 4))
+        head = Conv2D(64, kernel_size=(3, 3), activation='relu')(input)
+        conv1 = Conv2D(32, kernel_size=(3, 3), activation='relu')(head)
+        input_tail_network = Flatten()(conv1)
         dense1 = Dense(self.fc1_dims, activation='relu')(input_tail_network)
         dense2 = Dense(self.fc2_dims, activation='relu')(dense1)
         probs = Dense(self.n_actions, activation='softmax')(dense2)
         values = Dense(1, activation='linear')(dense2)
 
-        actor = Model(input=[input, delta], output=[probs])
+        actor = Model(input=[input], output=[probs])
         critic = Model(input=[input], output=[values])
         critic.summary()
         policy = Model(input=[input], output=[probs])
@@ -77,41 +90,68 @@ class Agent(object):
             
         self.entropy = 0
 
-        def custom_loss(y_true, y_pred):
+        def custom_entropy_loss(y_true, y_pred):
             out = K.clip(y_pred, 1e-5, 1-1e-5)
-            log_lik = y_true*K.log(out)
-            loss = K.sum(-log_lik*delta + 0.01 * self.entropy)
+            log_lik = K.log(out)*y_true
+            loss = K.sum(-log_lik + 0.01 * self.entropy)
             return loss
 
-        actor.compile(optimizer=Adam(lr=self.alpha), loss=custom_loss)
+        # actor.compile(optimizer=Adam(lr=self.alpha), loss="categorical_crossentropy")
+        actor.compile(optimizer=Adam(lr=self.alpha), loss=custom_entropy_loss)
         critic.compile(optimizer=Adam(lr=self.beta), loss='mean_squared_error')
 
         return actor, critic, policy
 
-    def choose_action(self, observation):
-        state = self.preprocess(observation)[np.newaxis, :]
+    def choose_action(self, stacked_observation):
+        state = stacked_observation[np.newaxis, :]
         probabilities = self.policy.predict(state)[0]
         action = np.random.choice(self.action_space, p=probabilities)
+
+        # print(probabilities)
         #Il clipping delle probabilità evita il logaritmo -inf
         self.entropy = - (tf.math.reduce_sum(probabilities * tf.math.log(tf.clip_by_value(probabilities,1e-5,1.0 - 1e-5))))
         return action
 
-    def learn(self, state, action, reward, state_, done):
-        state = self.preprocess(state)[np.newaxis,:]
-        state_ = self.preprocess(state_)[np.newaxis,:]
-        critic_value_ = self.critic.predict(state_)
-        critic_value = self.critic.predict(state)
+    def learn(self, current_stacked_state, action, reward, state_, done):
+        target = np.zeros((1, 1))
+        advantages = np.zeros((1, self.n_actions))
+        
+        state = current_stacked_state[np.newaxis,:]
+        s_, dump = self.stack_frames(state_)
+        state_ = s_[np.newaxis,:]
 
-        target = reward + self.gamma*critic_value_*(1-int(done))
-        delta =  target - critic_value
+        value = self.critic.predict(state)[0]
+        next_value = self.critic.predict(state_)[0]
 
-        actions = np.zeros([1, self.n_actions])
-        actions[np.arange(1), action] = 1
+        if done:
+            advantages[0][action] = reward - value
+            target[0][0] = reward
+        else:
+            advantages[0][action] = reward + self.gamma * next_value - value
+            target[0][0] = reward + self.gamma * next_value
 
-        # print(actions)
-        self.actor.fit([state, delta], actions, verbose=0)
+        #advantages = np.reshape(advantages, (1, advantages.shape[0], advantages.shape[1]))
+        #print(np.reshape(advantages, (advantages.shape[1])))
+        target = np.reshape(target, (1, target.shape[1]))
 
-        self.critic.fit(state, target, verbose=0)
+        self.actor.fit(state, advantages, epochs=1, verbose=0)
+        self.critic.fit(state, target, epochs=1, verbose=0)
+
+        # state_ = s_[np.newaxis,:]
+        # critic_value_ = self.critic.predict(state_)
+        # critic_value = self.critic.predict(state)
+
+
+        # target = reward + self.gamma*critic_value_*(1-int(done))
+        # delta =  target - critic_value
+
+        # actions = np.zeros([1, self.n_actions])
+        # actions[np.arange(1), action] = 1
+
+        # # print(actions)
+        # self.actor.fit([state, delta], actions, verbose=0)
+
+        # self.critic.fit(state, target, verbose=0)
 
     def save(self, envName):
         self.actor.save_weights(envName + '_actor.h5')        
--- main_keras_ac.py
+++ main_keras_ac.py
@@ -16,30 +16,34 @@ if __name__ == '__main__':
     #state_dimension = (84,84,1)
     n_actions = env.action_space.n
 
-    agent = Agent(n_actions=n_actions, input_dims = state_dimension, alpha=0.00001, beta=0.00005, gamma = 0.22, is_ram=False)
+    agent = Agent(n_actions=n_actions, input_dims = state_dimension, alpha=0.00001, beta=0.0005, gamma = 0.9)
 
     score_history = agent.score_history
     num_episodes = 5000
 
-    for i in range(num_episodes):
+    while len(agent.score_history) < num_episodes:
         done = False
         score = 0
         observation = env.reset()
-
+        # self.stacked_frames = deque([np.zeros((84, 84), dtype=np.int) for i in range(stack_size)], maxlen=4)
+        agent.stack_frames(observation, True)
         while not done:
             env.render()
-            action = agent.choose_action(observation)
+            stacked_observation, agent.stacked_frames = agent.stack_frames(observation)
+            action = agent.choose_action(stacked_observation)
             observation_, reward, done, info = env.step(action)
-            agent.learn(observation, action, reward, observation_, done)
+            agent.learn(stacked_observation, action, reward, observation_, done)
             observation = observation_
             score = score + reward
         agent.score_history.append(score)
-        agent.save(env_name)
+        if ((len(agent.score_history) % 10) == 0):
+            agent.save(env_name)
         env.close()
 
         avg_score = np.mean(score_history[-100:])
+        avg_10_score = np.mean(score_history[-10:])
         print('episode: ', len(agent.score_history),'score: %.2f' % score,
-              'avg score %.2f' % avg_score)
+              'avg last 100 episode score: %.2f' % avg_score, ' avg last 10 episode score: %.2f ' % avg_10_score)
 
     plotLearning(score_history, filename=env_name, window=100)
 
